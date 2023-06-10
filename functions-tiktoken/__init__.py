fbdfd35e295ad29f3bfe678ed77324f5cc636248
Shohei Nagata <7954174+shohei1029@users.noreply.github.com>
2023-06-10 17:30:41 +0900

    add tiktoken

--- functions-tiktoken/__init__.py
+++ functions-tiktoken/__init__.py
@@ -1,24 +1,31 @@
 import logging
 
 import azure.functions as func
+import tiktoken
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
-    if not name:
+    text = req.params.get("text")
+    if not text:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            text = req_body.get("text")
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if text:
+        enc = tiktoken.get_encoding(
+            "cl100k_base"
+        )  # for gpt-3.5-turbo, gpt-4, text-embedding-ada-002
+        tokens = enc.encode(text)
+        tokens_count = len(tokens)
+        logging.info(f"Tokens count: {tokens_count=}")
+        return func.HttpResponse(f"{tokens_count}")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a text in the query string or in the request body for a personalized response.",
+            status_code=200,
         )
