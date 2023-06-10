c1799feaae93e9b9e43752cfc0576a9bdc7557a8
Shohei Nagata <7954174+shohei1029@users.noreply.github.com>
2023-06-10 18:00:51 +0900

    great

--- functions-tiktoken/__init__.py
+++ functions-tiktoken/__init__.py
@@ -17,15 +17,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             text = req_body.get("text")
 
     if text:
-        enc = tiktoken.get_encoding(
+        enc: tiktoken.Encoding = tiktoken.get_encoding(
             "cl100k_base"
         )  # for gpt-3.5-turbo, gpt-4, text-embedding-ada-002
-        tokens = enc.encode(text)
-        tokens_count = len(tokens)
+        tokens: list[int] = enc.encode(text)
+        tokens_count: int = len(tokens)
         logging.info(f"Tokens count: {tokens_count=}")
         return func.HttpResponse(f"{tokens_count}")
     else:
         return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a text in the query string or in the request body for a personalized response.",
+            "no text provided",
             status_code=200,
         )
